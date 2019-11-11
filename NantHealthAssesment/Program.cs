ab6df1711556ddae403b4273def4ed5b9402927d
Damian Smug <smugdamian@gmail.com>
2019-11-11 21:27:33 +0000

    add: example program

--- NantHealthAssesment/Program.cs
+++ NantHealthAssesment/Program.cs
@@ -1,12 +1,44 @@
-﻿using System;
+﻿using NantHealthAssesment.Db;
+using NantHealthAssesmentLibrary.Model.Models.AnonymousUsers;
+using NantHealthAssesmentLibrary.Services;
+using System;
 
 namespace NantHealthAssesment
 {
     class Program
     {
+        AuthenticateService _authenticateService;
+        public Program()
+        {
+            _authenticateService = new AuthenticateService();
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Program program = new Program();
+            while (true)
+            {
+                program._TryLogin();
+            }
+        }
+
+        private void _TryLogin()
+        {
+            Console.Write("Type user: ");
+            var userName = Console.ReadLine();
+
+            Console.Write("Type password: ");
+            var password = Console.ReadLine();
+
+            var resultUser = _authenticateService.Authenticate(new AnonymousUser(userName, password));
+            if (resultUser != null)
+            {
+                Console.WriteLine("You are logged");
+            }
+            else
+            {
+                Console.WriteLine("Username or password are invalid");
+            }
         }
     }
 }
