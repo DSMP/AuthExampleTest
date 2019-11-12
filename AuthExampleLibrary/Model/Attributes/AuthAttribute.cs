6f106f24a0e737e5a4f111197204899b012ab90d
Damian Smug <smugdamian@gmail.com>
2019-11-12 08:46:39 +0000

    add: auth attribute

---
+++ AuthExampleLibrary/Model/Attributes/AuthAttribute.cs
@@ -0,0 +1,20 @@
+﻿using AuthExampleLibrary.Model.Models;
+using AuthExampleLibrary.Service.IServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AuthExampleLibrary.Model.Attributes
+{
+    [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = false)]
+    public class AuthAttribute : Attribute
+    {
+        public AuthAttribute(IAuthorize authorize, Claim claimsNeeded)
+        {
+            if (!authorize.Authorize(claimsNeeded))
+            {
+                throw new UnauthorizedAccessException();
+            }
+        }
+    }
+}
